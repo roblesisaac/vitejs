8529f82fdeab1d7fdfe277e3420598e29882ba38
roblesisaac <irobles1030@gmail.com>
2023-01-25 14:43:17 -0800

    243

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../static'
+    outDir: "../vitejs/static"
   },
   plugins: [vue()],
 })
