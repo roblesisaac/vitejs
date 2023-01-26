68c8c8525d43a59994e534a1e763d2f864686f2c
roblesisaac <irobles1030@gmail.com>
2023-01-26 11:58:44 -0800

    cloud update

--- api/auth.js
+++ api/auth.js
@@ -2,7 +2,7 @@ import { Pipe } from "../utils/pipe.js";
 import mongo from "../utils/mongo.js";
 
 export default async (req, res) => {
-  mongo.insert("users", {
+  mongo.insert.data({ res })("users", {
     name: 12
   }).then(output => {
     res.json(output);
