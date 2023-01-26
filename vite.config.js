e5db03dc3ac80f105fbd52a1643ccdfb80d2dcc1
roblesisaac <irobles1030@gmail.com>
2023-01-26 10:58:29 -0800

    cloud update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,13 @@ export default defineConfig({
   //     '/aloha': 'http://localhost:5173',
   //   },
   // },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      } 
+    })
+  ],
 })
