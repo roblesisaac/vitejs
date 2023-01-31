2459dd9dab23490aeadeac422451cb60212aae5c
roblesisaac <irobles1030@gmail.com>
2023-01-30 18:17:54 -0800

    cloud update

--- api/auth.js
+++ api/auth.js
@@ -5,37 +5,32 @@ import { Strategy }  from "passport-google-oauth20";
 import { params } from "@serverless/cloud";
 
 export default async (req, res, api) => {
-// Use the Passport middleware
-  api.use(passport.initialize());
-  api.use(passport.session());
+  // api.use(passport.initialize());
+  // api.use(passport.session());
 
   const respond = output => res.json(output);
 
-// Configure the Google Sign-In strategy
-passport.use(
-  new Strategy({
-    clientID: params.GOOGLE_ID,
-    clientSecret: params.GOOGLE_SECRET,
-    callbackURL: `https://pragmatic-binary-atdgy.cloud.serverless.com/auth/google/callback`
-  },
-  (accessToken, refreshToken, profile, cb) => {
-    console.log({accessToken, refreshToken, profile});
-    // Verify the user's identity
-    // Call the callback with the user's information
-    return cb(null, profile);
-  })
-);
+  passport.use(
+    new Strategy({
+      clientID: params.GOOGLE_ID,
+      clientSecret: params.GOOGLE_SECRET,
+      callbackURL: `https://pragmatic-binary-atdgy.cloud.serverless.com/auth/google/callback`
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log({accessToken, refreshToken, profile});
+      return cb(null, profile);
+    })
+  );
+  
+  // passport.serializeUser((user, cb) => {
+  //   cb(null, user);
+  // });
 
-// Serialize and deserialize the user for the session
-passport.serializeUser((user, cb) => {
-  cb(null, user);
-});
+  // passport.deserializeUser((obj, cb) => {
+  //   cb(null, obj);
+  // });
 
-passport.deserializeUser((obj, cb) => {
-  cb(null, obj);
-});
-
-passport.authenticate('google', { scope: ['profile'] });
+  // passport.authenticate('google', { scope: ['profile'] });
 
   // mongo.insert._import({ res, req })("users", { name: 12 })
   //   .then(respond)
