2b073d124b877714717374369513384e0a49e054
roblesisaac <irobles1030@gmail.com>
2023-01-31 17:58:29 -0800

    cloud update

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h1>Login</h1>
+      <p v-if="user">Hello {{ user.name }}</p>
+      <button v-if="!user" @click="login">Login with Google</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        user: null,
+      };
+    },
+    methods: {
+      login() {
+        window.location.href = '/auth/google';
+      },
+    },
+    created() {
+    //   fetch('/api/user')
+    //     .then(res => res.json())
+    //     .then(user => {
+    //       this.user = user;
+    //     });
+    },
+  };
+  </script>
+  
