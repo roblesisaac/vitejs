bab63bfea7471d9a06b975554d5d8c2588e39649
roblesisaac <irobles1030@gmail.com>
2023-01-31 19:01:13 -0800

    cloud update

--- src/main.js
+++ src/main.js
@@ -27,6 +27,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     console.log({ to })
     if (to.fullPath === '/login/auth/google') {
+        console.log("nope");
         return;
     } else {
         next();
