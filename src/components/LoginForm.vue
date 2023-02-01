0f967591281f6e3c2b849c329138093b0131c7c1
roblesisaac <irobles1030@gmail.com>
2023-01-31 18:10:50 -0800

    cloud update

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,22 +1,23 @@
 <template>
-<div>
-    <h1>Login</h1>
-    <p v-if="user">Hello {{ user.name }}</p>
-    <button v-if="!user" @click="login">Login with Google</button>
-</div>
-</template>
+    <div>
+      <h1>Login</h1>
+      <p v-if="user">Hello {{ user.name }}</p>
+      <button v-if="!user" @click="login">Login with Google</button>
+    </div>
+  </template>
   
   <script>
+  import { useRoute } from 'vue-router';
+  
   export default {
-    data() {
+    setup() {
+      const { path } = useRoute();
       return {
         user: null,
+        login() {
+          window.location.href = '/login/auth/google?redirect=' + encodeURIComponent(path);
+        },
       };
-    },
-    methods: {
-      login() {
-        window.location.href += '/auth/google';
-      },
     },
     created() {
     //   fetch('/api/user')
@@ -26,5 +27,4 @@
     //     });
     },
   };
-  </script>
-  
+  </script>  
