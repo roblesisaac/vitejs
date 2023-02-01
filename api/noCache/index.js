43c0bc6c7c46d68bf87523a458b325c91c172b9f
roblesisaac <irobles1030@gmail.com>
2023-02-01 15:15:27 -0800

    cloud update

--- api/noCache/index.js
+++ api/noCache/index.js
@@ -1,4 +1,5 @@
 export default function(req, res, next) {
+    console.log("not caching");
     res.header("Cache-Control", "no-cache, no-store, must-revalidate");
     res.header("Pragma", "no-cache");
     res.header("Expires", 0);
